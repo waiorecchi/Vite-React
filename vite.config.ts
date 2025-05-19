9adc365d1dc9c703d11692a3b09559e032e2b86d
waiorecchi <tatukikitamura123456@gmail.com>
2025-05-19 12:25:27 +0900

    Viteの設定ファイルを更新し、baseオプションを追加。サーバー設定をコメントアウトしました。

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
+  base:"/Vite-React/",
+  //server: {
+  // port: 3000,
+  // open: true,
   },
-})
+)
 
 
